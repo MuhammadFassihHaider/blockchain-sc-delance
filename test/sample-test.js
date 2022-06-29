f1255c6d81c68c341e4d4a18523dffc178352b88
Fassih Haider <fassih.haider786@gmail.com>
2022-06-29 17:43:54 +0500

    test: update test file by adding a few tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,40 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const initializeContract = async () => {
+    const freelancerAddress = "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2";
+    const deadline = 1664433591;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const Delance = await ethers.getContractFactory("Delance");
+    const delance = await Delance.deploy(freelancerAddress, deadline);
+    await delance.deployed();
+    return { delance, freelancerAddress, deadline };
+}
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+describe("Delance", function () {
+    it("Should initialize values in constructor", async () => {
+        const { delance, deadline, freelancerAddress } = await initializeContract();
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+        expect(await delance.freelancer()).to.equal(freelancerAddress);
+        expect(await delance.deadline()).to.equal(deadline);
+    });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+    it("Should correctly set deadline", async () => {
+        const { delance, deadline } = await initializeContract();
+
+        const setDeadline = await delance.setDeadline(deadline + 5)
+        await setDeadline.wait()
+
+        expect(await delance.deadline()).to.equal(deadline + 5);
+    });
+
+    it("Should correctly set freelancerAddress", async () => {
+        const { delance, freelancerAddress } = await initializeContract();
+        const updatedFreelancerAddress = "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db";
+        const setFreelancer = await delance.setAddress(updatedFreelancerAddress)
+        await setFreelancer.wait();
+
+        expect(await delance.freelancer()).to.equal(updatedFreelancerAddress)
+
+    })
 });
