b8a5f9bb8a5508e50a020d7ad7366f4ac267d2ad
Fassih Haider <fassih.haider786@gmail.com>
2022-07-01 11:57:00 +0500

    feat(client): setup contract with types

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,13 @@
+import Web3 from "web3";
+import { contractAbi, contractAddress } from "./utils/constants";
+import { Delance } from "../../typechain-types/Delance";
+const web3 = new Web3("ws://localhost:8545");
+
+const delanceContract: Delance = new web3.eth.Contract(
+    contractAbi,
+    contractAddress
+) as any;
+
 function App() {
     return (
         <div className="bg-red-300">
