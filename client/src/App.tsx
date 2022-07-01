ad121b29a5c704e5c817f798ae5e1b613f3130da
Fassih Haider <fassih.haider786@gmail.com>
2022-07-01 13:04:30 +0500

    feat(client): add dummy functionality for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,31 @@
+import { useEffect } from "react";
 import Web3 from "web3";
+import { AbiItem } from "web3-utils";
 import { contractAbi, contractAddress } from "./utils/constants";
-import { Delance } from "../../typechain-types/Delance";
+import { Delance } from "./generated-contract-types/contracts/Delance";
+
 const web3 = new Web3("ws://localhost:8545");
 
 const delanceContract: Delance = new web3.eth.Contract(
-    contractAbi,
+    contractAbi as AbiItem[],
     contractAddress
 ) as any;
 
 function App() {
+    async function getRequests() {
+        const freelancer = await delanceContract.methods.freelancer().call();
+        const employer = await delanceContract.methods.employer().call();
+
+        console.log({ freelancer, employer });
+        const requestResponse = await delanceContract.methods
+            .createRequest("10000000000000000000", "FE completed")
+            .send({ from: employer });
+        const requests = await delanceContract.methods.getRequests().call();
+        console.log({ requests, requestResponse });
+    }
+    useEffect(() => {
+        getRequests();
+    }, []);
     return (
         <div className="bg-red-300">
             <h1 className="bg-blue-200">hello</h1>
--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
 import Delance from "./Delance.json";
 
-export const contractAbi: any = Delance.abi;
+export const contractAbi = Delance.abi;
 export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
